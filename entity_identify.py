8d5a75ac33dd7e79d839ebf2fdba6aea4df6a41a
FengjinChen <1871653365@qq.com>
2017-02-12 22:09:11 +0800

    not finished

--- entity_identify.py
+++ entity_identify.py
@@ -6,16 +6,13 @@ import json
 
 import exact_match.mm
 import fuzzy_match.fuzzy_match
-import pinyin_match.mm
 import pypinyin
 
-
-def preprocess_chinese_sentence(sent):
-    pass
-
-def chinese_word2pinyin(word_list):
-    pass
-
+#
+# def chinese_word2pinyin(word_list):
+#     pinyin_word=[]
+#     for word in word_list:
+#         yield pypinyin.pinyin(list(word),pypinyin.NORMAL)
 
 
 def questions_from_json(filename):
@@ -32,20 +29,56 @@ def questions_from_json(filename):
     return result
 
 
+def decode_entity_type(word, reverse=False):
+    if reverse:
+        word = word[::-1]
+    return word[0]
+
+
+def get_word_list(entity_name_file, pinyin):
+    f = open(entity_name_file, 'r')
+    json_file = json.load(f)
+    for word in json_file:
+        entity_type = encode_entity_type(json_file[word])
+        entity_with_type = [entity_type]
+        if pinyin:
+            entity_with_type.extend(pypinyin.pinyin(word,
+                                                    style=pypinyin.NORMAL))
+        else:
+            entity_with_type.extend(list(word))
+        yield entity_with_type, rtt
+    f.close()
+
+
+def encode_entity_type(id_list):
+    entity_types = []
+    for entity_id in id_list:
+        print entity_id
+        if entity_id[1].isdigit():
+
+            if entity_id[0] not in entity_types:
+                entity_types.append(entity_id[0])
+        else:
+            if entity_id[:2] not in entity_types:
+                entity_types.append(entity_id[:2])
+
+    return '@' + '-'.join(sorted(entity_types)) + '@'
+
+
 def entity_identify():
     entity_name_file = 'data/name-idlist-dict-all.json'
 
-    bseg = exact_match.mm.BMMSeg()
-    words_exact = bseg.get_word_list(entity_name_file)
-    bseg.add_words(words_exact)
+    words_exact = get_word_list(entity_name_file, pinyin=False)
+    exact_bseg = exact_match.mm.BMMSeg()
+    exact_bseg.add_words(words_exact, encode_entity_type)
+
+    words_pinyin = get_word_list(entity_name_file, pinyin=True)
+    pinyin_bseg = exact_match.mm.BMMSeg()
+    pinyin_bseg.add_words(words_pinyin, encode_entity_type)
 
     fuzzy = fuzzy_match.fuzzy_match.FuzzyMatch()
     words_fuzzy = fuzzy.get_word_list(entity_name_file)
     fuzzy.add_words(words_fuzzy)
-
-    pinyin = pinyin_match.mm.BMMSeg()
-    words_pinyin = pinyin.get_word_list(entity_name_file)
-    pinyin.add_words(words_pinyin)
 
     output_file_name = 'data/entity_identify.csv'
     csvfile = open(output_file_name, 'wb')
@@ -55,9 +88,9 @@ def entity_identify():
     question_file_name = 'data/qa3.json'
     questions = questions_from_json(question_file_name)
     for question in questions:
-        exact_entities = bseg.entity_identify(question)
+        exact_entities = exact_bseg.entity_identify(question)
         fuzzy_entities = fuzzy.entity_identify(question)
-        pinyin_entities = pinyin.entity_identify(question)
+        # pinyin_entities = pinyin.entity_identify(question)
         one_line = []
         for entity in exact_entities:
             if entity not in one_line:
@@ -65,9 +98,9 @@ def entity_identify():
         for entity in fuzzy_entities:
             if entity not in one_line:
                 one_line.append(entity)
-        for entity in pinyin_entities:
-            if entity not in one_line:
-                one_line.append(entity)
+        # for entity in pinyin_entities:
+        #     if entity not in one_line:
+        #         one_line.append(entity)
         one_line_with_question = [question]
         one_line_with_question.extend(one_line)
         print one_line_with_question[0]
@@ -76,4 +109,11 @@ def entity_identify():
 
 
 if __name__ == "__main__":
-    entity_identify()
+    # words = get_word_list('data/name-idlist-dict-all.json', pinyin=False)
+    # for word in words:
+    #     for character in word:
+    #         print character,
+    #     print ""
+    # entity_identify()
+
+    print
--- exact_match/mm.py
+++ exact_match/mm.py
@@ -9,17 +9,6 @@
 # Copyright (c) 2016 www.drcubic.com, Inc. All Rights Reserved
 #
 ########################################################################
-# -*- coding: utf-8 -*-
-# PyCi
-#
-# Copyright (c) 2009, The PyCi Project
-# Authors: Wu Ke <ngu.kho@gmail.com>
-#          Chen Xing <cxcxcxcx@gmail.com>
-# URL: <http://code.google.com/p/pyci>
-# For license information, see COPYING
-"""Forward and Backward Maximum Matching word segmentor. Mainly used
-as a baseline segmentor.
-"""
 __all__ = ["FMMSeg", "BMMSeg"]
 
 import json
@@ -30,9 +19,9 @@ from trie import Trie, encode_word
 
 reload(sys)
 sys.setdefaultencoding('utf8')
-
 """Note: the sentence and token is a list of single word.
 """
+
 
 class FMMSeg(object):
     """A forward maximum matching Chinese word segmentor.
@@ -74,7 +63,6 @@ class FMMSeg(object):
             else:
                 self._trie[encode_word(word[1:])] = value(word)
 
-
     def entity_identify(self, sent):
         """Replace medical entity in sent with common words.
 
@@ -91,7 +79,8 @@ class FMMSeg(object):
             if idx is not None:
                 word = encode_word(sent[offset:idx])
                 entity_type = self._trie[word]
-                entity_location_with_types.append([offset, idx-1, entity_type])
+                entity_location_with_types.append([offset, idx - 1, entity_type
+                                                   ])
                 offset = idx
             else:
                 offset += 1
@@ -127,8 +116,7 @@ class BMMSeg(FMMSeg):
         @return: sentence with common words.
         """
         sent = sent[::-1]
-        entity_location_with_types = FMMSeg.entity_identify(
-            self, sent)
+        entity_location_with_types = FMMSeg.entity_identify(self, sent)
         return entity_location_with_types
 
     def get_token_value(self, token):
@@ -137,25 +125,4 @@ class BMMSeg(FMMSeg):
         token = token[::-1]
         return FMMSeg.get_token_value(token)
 
-
 # In[ ]:
-
-
-def decode_entity_type(word, reverse=False):
-    if reverse:
-        word = word[::-1]
-    return word[0]
-
-
-def encode_entity_type(id_list):
-    entity_types = []
-    for entity_id in id_list:
-        if entity_id[1].isdigit():
-            if entity_id[0] not in entity_types:
-                entity_types.append(entity_id[0])
-        else:
-            if entity_id[:2] not in entity_types:
-                entity_types.append(entity_id[:2])
-
-    return '@' + '-'.join(sorted(entity_types)) + '@'
-
