e1f9e431cba977e797ca2a0ea136b97860e8b29c
FengjinChen <1871653365@qq.com>
2017-02-13 11:57:31 +0800

    11:57的提交

--- entity_identify.py
+++ entity_identify.py
@@ -38,6 +38,8 @@ def hanzi_list2pinyin(hanzi_list):
 def get_word_list(entity_name_file):
     f = open(entity_name_file, 'r')
     json_file = json.load(f)
+    hanzi_list_result = []
+    pinyin_list_result = []
     for word in json_file:
         entity_type = encode_entity_type(json_file[word])
         hanzi_list = [entity_type, ]
@@ -45,15 +47,16 @@ def get_word_list(entity_name_file):
         word_list = list(word)
         hanzi_list.extend(word_list)
         pinyin_list.extend(hanzi_list2pinyin(word_list))
-
-        yield hanzi_list, pinyin_list
+        hanzi_list_result.append(hanzi_list)
+        pinyin_list_result.append(pinyin_list)
+    return hanzi_list_result, pinyin_list_result
     f.close()
 
 
 def encode_entity_type(id_list):
     entity_types = []
     for entity_id in id_list:
-        print entity_id
+        # print entity_id
         if entity_id[1].isdigit():
 
             if entity_id[0] not in entity_types:
@@ -66,36 +69,38 @@ def encode_entity_type(id_list):
 
 
 def entity_extract(entity_info, question_hanzi_list):
+
     entity_with_type = []
     for loc_with_type in entity_info:
         entity_type = loc_with_type[2]
-        entity = question_hanzi_list[entity_info[0]:entity_info[1] + 1]
+        entity = question_hanzi_list[loc_with_type[0]:loc_with_type[1] + 1]
+        print ''.join(entity)
         entity_with_type.append(''.join(entity) + '/' + entity_type)
     return entity_with_type
 
 
 def entity_identify(argc, argv):
-    if argc < 4:
-        print(
-            "Usage:%s <entity_name_file> <output_file_name> <question_file_name>"
-            % (argv[0]))
-    # entity_name_file = 'data/name-idlist-dict-all.json'
-    # output_file_name = 'data/entity_identify.csv'
-    # question_file_name = 'data/qa3.json'
-    entity_name_file = argv[1]
-    output_file_name = argv[2]
-    question_file_name = argv[3]
+    # if argc < 4:
+    #     print(
+    #         "Usage:%s <entity_name_file> <output_file_name> <question_file_name>"
+    # #         % (argv[0]))
+    # entity_name_file = argv[1]
+    # output_file_name = argv[2]
+    # question_file_name = argv[3]
+    entity_name_file = 'data/name-idlist-dict-all.json'
+    output_file_name = 'data/entity_identify.csv'
+    question_file_name = 'data/qa3.json'
 
-    words_exact = get_word_list(entity_name_file)
+    hanzi_list, pinyin_list = get_word_list(entity_name_file)
     hanzi_bseg = exact_match.mm.BMMSeg()
-    hanzi_bseg.add_words(words_exact[0], decode_entity_type)
+    hanzi_bseg.add_words(hanzi_list, decode_entity_type)
 
     pinyin_bseg = exact_match.mm.BMMSeg()
-    pinyin_bseg.add_words(words_exact[1], decode_entity_type)
+    pinyin_bseg.add_words(pinyin_list, decode_entity_type)
 
-    fuzzy = fuzzy_match.fuzzy_match.FuzzyMatch()
-    words_fuzzy = fuzzy.get_word_list(entity_name_file)
-    fuzzy.add_words(words_fuzzy)
+    # fuzzy = fuzzy_match.fuzzy_match.FuzzyMatch()
+    # words_fuzzy = fuzzy.get_word_list(entity_name_file)
+    # fuzzy.add_words(words_fuzzy)
 
     csvfile = open(output_file_name, 'wb')
     csvfile.write(codecs.BOM_UTF8)
@@ -103,22 +108,26 @@ def entity_identify(argc, argv):
 
     questions = questions_from_json(question_file_name)
     for question in questions:
+        one_line = []
         question_hanzi_list = list(question)
         hanzi_entity_info = hanzi_bseg.entity_identify(question_hanzi_list)
         question_pinyin_list = hanzi_list2pinyin(question_hanzi_list)
+        # print "question:",question_pinyin_list
+        # print question_hanzi_list
         pinyin_entity_info = pinyin_bseg.entity_identify(question_pinyin_list)
         hanzi_entity_result = entity_extract(hanzi_entity_info,
                                              question_hanzi_list)
         pinyin_entity_result = entity_extract(pinyin_entity_info,
                                               question_hanzi_list)
 
-        fuzzy_entities_result = fuzzy.entity_identify(question)
+        # fuzzy_entities_result = fuzzy.entity_identify(question)
 
-        one_line = []
-
+        # print fuzzy_entities_result
+        # print pinyin_entity_result
+        # print hanzi_entity_result
         one_line_with_question = [question]
         one_line_with_question.extend(one_line)
-        print one_line_with_question[0]
+        # print one_line_with_question[0]
         csvwriter.writerow(one_line_with_question)
     csvfile.close()
 
