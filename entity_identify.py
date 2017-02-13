575c13610d1c18d3f67737dc722da313a06af255
elias-1 <shileicao@stu.xjtu.edu.cn>
2017-02-13 09:40:51 +0800

    refine code

--- entity_identify.py
+++ entity_identify.py
@@ -8,11 +8,7 @@ import exact_match.mm
 import fuzzy_match.fuzzy_match
 import pypinyin
 
-#
-# def chinese_word2pinyin(word_list):
-#     pinyin_word=[]
-#     for word in word_list:
-#         yield pypinyin.pinyin(list(word),pypinyin.NORMAL)
+
 
 
 def questions_from_json(filename):
@@ -25,8 +21,9 @@ def questions_from_json(filename):
     with open(filename) as jsonfile:
         questions_json = json.load(jsonfile)
         for key in questions_json.keys():
-            result.extend(questions_json[key])
-    return result
+            for question in questions_json[key]:
+                yield question
+
 
 
 def decode_entity_type(word, reverse=False):
@@ -34,19 +31,22 @@ def decode_entity_type(word, reverse=False):
         word = word[::-1]
     return word[0]
 
+def hanzi_list2pinyin(hanzi_list):
+    return [pypinyin.pinyin(word, style=pypinyin.NORMAL)[0][0] for word in hanzi_list]
 
-def get_word_list(entity_name_file, pinyin):
+
+def get_word_list(entity_name_file):
     f = open(entity_name_file, 'r')
     json_file = json.load(f)
     for word in json_file:
         entity_type = encode_entity_type(json_file[word])
-        entity_with_type = [entity_type]
-        if pinyin:
-            entity_with_type.extend(pypinyin.pinyin(word,
-                                                    style=pypinyin.NORMAL))
-        else:
-            entity_with_type.extend(list(word))
-        yield entity_with_type, rtt
+        hanzi_list = [entity_type,]
+        pinyin_list = [entity_type,]
+        word_list = list(word)
+        hanzi_list.extend(word_list)
+        pinyin_list.extend(hanzi_list2pinyin(word_list))
+
+        yield hanzi_list, pinyin_list
     f.close()
 
 
@@ -65,16 +65,25 @@ def encode_entity_type(id_list):
     return '@' + '-'.join(sorted(entity_types)) + '@'
 
 
+def entity_extract(entity_info, question_hanzi_list):
+    entity_with_type = []
+    for loc_with_type in entity_info:
+        entity_type = loc_with_type[2]
+        entity = question_hanzi_list[entity_info[0]:entity_info[1]+1]
+        entity_with_type.append(entity + '/' + entity_type)
+    return entity_with_type
+
+
 def entity_identify():
     entity_name_file = 'data/name-idlist-dict-all.json'
 
-    words_exact = get_word_list(entity_name_file, pinyin=False)
-    exact_bseg = exact_match.mm.BMMSeg()
-    exact_bseg.add_words(words_exact, encode_entity_type)
+    words_exact = get_word_list(entity_name_file)
+    hanzi_bseg = exact_match.mm.BMMSeg()
+    hanzi_bseg.add_words(words_exact[0], decode_entity_type)
 
-    words_pinyin = get_word_list(entity_name_file, pinyin=True)
+
     pinyin_bseg = exact_match.mm.BMMSeg()
-    pinyin_bseg.add_words(words_pinyin, encode_entity_type)
+    pinyin_bseg.add_words(words_exact[1], decode_entity_type)
 
     fuzzy = fuzzy_match.fuzzy_match.FuzzyMatch()
     words_fuzzy = fuzzy.get_word_list(entity_name_file)
@@ -88,19 +97,20 @@ def entity_identify():
     question_file_name = 'data/qa3.json'
     questions = questions_from_json(question_file_name)
     for question in questions:
-        exact_entities = exact_bseg.entity_identify(question)
+        question_hanzi_list = list(question)
+        hanzi_entity_info = hanzi_bseg.entity_identify(question_hanzi_list)
+        question_pinyin_list = hanzi_list2pinyin(question_hanzi_list)
+        pinyin_entity_info = pinyin_bseg.entity_identify(question_pinyin_list)
         fuzzy_entities = fuzzy.entity_identify(question)
-        # pinyin_entities = pinyin.entity_identify(question)
+
         one_line = []
-        for entity in exact_entities:
-            if entity not in one_line:
-                one_line.append(entity)
+        hanzi_entity_result = entity_extract(hanzi_entity_info, question_hanzi_list)
+        pinyin_entity_result = entity_extract(hanzi_entity_info, question_hanzi_list)
+
         for entity in fuzzy_entities:
             if entity not in one_line:
                 one_line.append(entity)
-        # for entity in pinyin_entities:
-        #     if entity not in one_line:
-        #         one_line.append(entity)
+
         one_line_with_question = [question]
         one_line_with_question.extend(one_line)
         print one_line_with_question[0]
@@ -109,11 +119,6 @@ def entity_identify():
 
 
 if __name__ == "__main__":
-    # words = get_word_list('data/name-idlist-dict-all.json', pinyin=False)
-    # for word in words:
-    #     for character in word:
-    #         print character,
-    #     print ""
-    # entity_identify()
+    entity_identify()
 
-    print
+
