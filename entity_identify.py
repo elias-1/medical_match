a9824148426a8365d65d2c0ffdd6384a8cc17a57
FengjinChen <1871653365@qq.com>
2017-02-15 21:12:01 +0800

    bug to fix

--- entity_identify.py
+++ entity_identify.py
@@ -3,6 +3,7 @@
 import codecs
 import csv
 import json
+import pprint
 import sys
 
 import exact_match.mm
@@ -53,6 +54,17 @@ def get_word_list(entity_name_file):
     return hanzi_list_result, pinyin_list_result
 
 
+def get_common_word(filename):
+    """
+    从文件中获取common word词表
+    :param filename: common word文件，一个json文件
+    :return: 一个包含common word词表的list，每个词是unicode形式
+    """
+    json_file = open(filename, 'r')
+    common_word_list = json.load(json_file)
+    return common_word_list['data']
+
+
 def encode_entity_type(id_list):
     entity_types = []
     for entity_id in id_list:
@@ -79,56 +91,79 @@ def entity_extract(entity_info, question_hanzi_list):
 
 
 def entity_identify(argc, argv):
-    # if argc < 4:
-    #     print(
-    #         "Usage:%s <entity_name_file> <output_file_name> <question_file_name>"
-    # #         % (argv[0]))
-    # entity_name_file = argv[1]
-    # output_file_name = argv[2]
-    # question_file_name = argv[3]
-    entity_name_file = 'data/name-idlist-dict-all.json'
-    output_file_name = 'data/entity_identify.csv'
-    question_file_name = 'data/qa3.json'
+    if argc < 4:
+        print(
+            "Usage:%s <entity_name_file> <output_file_name> <question_file_name>"
+            % (argv[0]))
+    entity_name_file = argv[1]
+    output_file_name = argv[2]
+    question_file_name = argv[3]
+    common_words_file = argv[4]
+    # entity_name_file = 'data/name-idlist-dict-all.json'
+    # output_file_name = 'data/entity_identify_80_percentage.csv'
+    # question_file_name = 'data/qa3.json'
+    # common_words_file='data/merge_split2.json'
 
     hanzi_list, pinyin_list = get_word_list(entity_name_file)
-    # print hanzi_list
     hanzi_bseg = exact_match.mm.BMMSeg()
     hanzi_bseg.add_words(hanzi_list, decode_entity_type)
 
     pinyin_bseg = exact_match.mm.BMMSeg()
     pinyin_bseg.add_words(pinyin_list, decode_entity_type)
 
-    fuzzy = fuzzy_match.fuzzy_match.FuzzyMatch()
+    fuzzy = fuzzy_match.fuzzy_match.FuzzyMatch(threshold=80)
+    common_words = get_common_word(common_words_file)
+    fuzzy.add_common_words(common_words)
+
     words_fuzzy = fuzzy.get_word_list(entity_name_file)
     fuzzy.add_words(words_fuzzy)
 
     csvfile = open(output_file_name, 'wb')
     csvfile.write(codecs.BOM_UTF8)
     csvwriter = csv.writer(csvfile)
-
+    # #
     questions = questions_from_json(question_file_name)
     for question in questions:
         one_line = []
         question_hanzi_list = list(question)
         hanzi_entity_info = hanzi_bseg.entity_identify(question_hanzi_list)
-
         question_pinyin_list = hanzi_list2pinyin(question_hanzi_list)
         pinyin_entity_info = pinyin_bseg.entity_identify(question_pinyin_list)
-
         hanzi_entity_result = entity_extract(hanzi_entity_info,
                                              question_hanzi_list)
-        # print '%s   %s' % (question, '##'.join(hanzi_entity_result))
         pinyin_entity_result = entity_extract(pinyin_entity_info,
                                               question_hanzi_list)
 
-        fuzzy_entities_result = fuzzy.entity_identify(question)
+        fuzzy_entity_result = fuzzy.entity_identify(question)
+        fuzzy_pinyin_entity_result = fuzzy.pinyin_entity_identify(question)
 
-        print '%s   %s' % (question, '\t'.join(fuzzy_entities_result))
-        # print pinyin_entity_result
-        # print hanzi_entity_result
         one_line_with_question = [question]
-        one_line_with_question.extend(one_line)
-        # print one_line_with_question[0]
+        oneline = []
+        #########################################################################
+        print question
+        print "hanzi:",
+        for i in hanzi_entity_result:
+            print i
+        print 'pinyin:',
+        for i in pinyin_entity_result:
+            print i
+        print "fuzzy",
+        for i in fuzzy_entity_result:
+            print i
+        print "fuzzy_pinyin",
+        for i in fuzzy_pinyin_entity_result:
+            print i
+        #########################################################################
+
+        one_line.extend(hanzi_entity_result)
+        one_line.extend(pinyin_entity_result)
+        one_line.extend(fuzzy_entity_result)
+        one_line.extend(fuzzy_pinyin_entity_result)
+        one_line = set(one_line)
+        one_line_with_question.extend(list(one_line))
+        for i in one_line_with_question:
+            print i,
+        print ''
         csvwriter.writerow(one_line_with_question)
     csvfile.close()
 
--- fuzzy_match/fuzzy_match.py
+++ fuzzy_match/fuzzy_match.py
@@ -4,14 +4,18 @@
 import json
 
 import jieba
+import pypinyin
 from fuzzywuzzy.fuzz import ratio
 
 
 class FuzzyMatch(object):
-    def __init__(self, word_list=None):
-        self.word_list = word_list
+    def __init__(self, threshold=50):
+        self._word_list = None
+        self._threshold = threshold
+        self._common_list = None
 
-    def get_word_list(self, filename):
+    @staticmethod
+    def get_word_list(filename):
         """
             extract medical words and its class from a file and return them
         :param filename:a json file that contains words and its class.
@@ -27,49 +31,52 @@ class FuzzyMatch(object):
                 name_with_type = {}
                 name_with_type['name'] = entity_name
                 name_with_type['type'] = entity_names[entity_name]
+                name_with_type['pinyin'] = " ".join(
+                    a[0]
+                    for a in pypinyin.pinyin(entity_name,
+                                             style=pypinyin.NORMAL))
                 names_with_type.append(name_with_type)
             return names_with_type
 
     def add_words(self, word_list):
-        self.word_list = word_list
+        self._word_list = word_list
 
-    def _most_similar_words(self, key_word, limit=0, threshold=80):
+    def add_common_words(self, common_word_list):
+        self._common_list = common_word_list
+
+    def _most_similar_words(self, key_word, pinyin=False):
         """
+
         :param key_word: word that need to match,string
-        :param word_list:words to search, a list of lists
+        :param _word_list:words to search, a list of lists
         :param limit: If limit==0,than return all words whose ratio is larger than threshold,if limit == 1,than return the
         most similar word. If some words has the same similarity and word number is more than limit, than word at the front
-        of word_list
+        of _word_list
          is returned. Notice that if no word has similarity higher than threshold,than None will be returned
         :param threshold:if treshold is zero,than the most similar word will be returned, else words whose ratio more than
         threshold will be returned.
-        :return: Words in word_list that is most similar to key_word. More information, see para limit
+        :return: Words in _word_list that is most similar to key_word. More information, see para limit
 
         """
-        if limit == 1:
-            result = None
-            max_ratio = 0
-            for word in self.word_list:
-                # print word
+        result = []
+        if pinyin:
+            key_word = " ".join(a[0]
+                                for a in pypinyin.pinyin(
+                                    key_word, style=pypinyin.NORMAL))
+            for word in self._word_list:
+                word_ratio = ratio(key_word, word['pinyin'])
+                if word_ratio > self._threshold:
+                    result.append(word['name'] + '/' +
+                                  self._encode_entity_type(word['type']))
+        else:
+            for word in self._word_list:
                 word_ratio = ratio(key_word, word['name'])
-                if word_ratio > max_ratio:
-                    result = word
-                    max_ratio = word_ratio
-            if max_ratio < threshold:
-                return None
-            else:
-                return [word['name'] + self._encode_entity_type(word['type'])]
-        elif limit == 0:
-            result = []
-            for word in self.word_list:
-                # print word
-                word_ratio = ratio(key_word, word['name'])
-                if word_ratio > threshold:
-                    result.append(word['name'] + self._encode_entity_type(word[
-                        'type']))
-            return None if len(result) == 0 else result
+                if word_ratio > self._threshold:
+                    result.append(word['name'] + '/' +
+                                  self._encode_entity_type(word['type']))
+        return None if len(result) == 0 else result
 
-    def _encode_entity_type(id_list):
+    def _encode_entity_type(self, id_list):
         entity_types = []
         for entity_id in id_list:
             if entity_id[1].isdigit():
@@ -79,21 +86,27 @@ class FuzzyMatch(object):
                 if entity_id[:2] not in entity_types:
                     entity_types.append(entity_id[:2])
 
-        return '@' + '+'.join(sorted(entity_types))
+        return '@' + '-'.join(sorted(entity_types)) + '@'
 
-    def entity_identify(self, sentence, threshold=50):
+    def entity_identify(self, sentence, pinyin=False):
         """
         Find entities in a sentence using fuzzy match
         :param sentence:sentence to identify
         :param threshold:if threshold is zero,than the most similar word will be
             returned, else words whose ratio more than threshold will be returned.
+        :param pinyin
         :return:
         """
         sentence_word = jieba.cut(sentence)
+        sentence_word = [word for word in sentence_word
+                         if word not in self._common_list]
         entities_with_type = []
         for word in sentence_word:
             if len(word) > 1:
-                word_matched = self._most_similar_words(word, self.word_list)
+                word_matched = self._most_similar_words(word, pinyin=True)
                 if word_matched:
                     entities_with_type.extend(word_matched)
         return entities_with_type
+
+    def pinyin_entity_identify(self, sentence):
+        return self.entity_identify(sentence, pinyin=True)
