df6520908dafce539b8e898b99f0b0aa214295f3
elias-1 <shileicao@stu.xjtu.edu.cn>
2017-02-22 21:21:33 +0800

    sentence clfier refine

--- clfier/sentence_clfier.py
+++ clfier/sentence_clfier.py
@@ -67,7 +67,7 @@ class SentenceClfier:
         session_conf = tf.ConfigProto(
             allow_soft_placement=FLAGS.allow_soft_placement,
             log_device_placement=FLAGS.log_device_placement)
-        self.sess = tf.Session(config=session_conf, graph=graph)
+        self.sess = tf.Session(config=session_conf)
 
         checkpoint_file = tf.train.latest_checkpoint(
             os.path.join(FLAGS.run_dir, 'checkpoints'))
