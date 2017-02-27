776f6205fcc47c2b6f4d336e12d5d21bd6580929
elias-1 <shileicao@stu.xjtu.edu.cn>
2017-02-27 15:45:54 +0800

    refine code

--- es_match/es_input.py
+++ es_match/es_input.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2016 www.drcubic.com, Inc. All Rights Reserved
+# Copyright (c) 2017 www.drcubic.com, Inc. All Rights Reserved
 #
 """
 File: es_input.py
@@ -10,24 +10,42 @@ Date: 17-2-26 下午8:15
 """
 import json
 
-from es_match import create_index, hanzi2pinyin
+from es_match import create_index, encode_pinyin
 
 
-def insert2es(words):
+def encode_entity_type(id_list):
+    entity_types = []
+    for entity_id in id_list:
+        # print entity_id
+        if entity_id[1].isdigit():
+
+            if entity_id[0] not in entity_types:
+                entity_types.append(entity_id[0])
+        else:
+            if entity_id[:2] not in entity_types:
+                entity_types.append(entity_id[:2])
+
+    return '@' + '-'.join(sorted(entity_types)) + '@'
+
+
+def insert2es(entitys_with_types):
     id = 0
-    for word in words:
+    for word, entity_type in entitys_with_types:
         doc = {}
         doc['Name'] = word
-        doc['Pinyin'] = ' '.join(hanzi2pinyin(word))
+        doc['Pinyin'] = encode_pinyin(word)
+        doc['Entity_type'] = entity_type
         create_index(id, doc)
         id += 1
 
 
 if __name__ == "__main__":
     words = []
+    entity_types = []
     with open('../data/name-idlist-dict-all.json', 'r') as f:
         data = json.load(f)
         for key in data.keys():
             words.append(key)
+            entity_types.append(encode_entity_type(data[key]))
 
-    insert2es(words)
+    insert2es(zip(words, entity_types))
--- es_match/es_match.py
+++ es_match/es_match.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2016 www.drcubic.com, Inc. All Rights Reserved
+# Copyright (c) 2017 www.drcubic.com, Inc. All Rights Reserved
 #
 """
 File: es_match.py
@@ -9,8 +9,7 @@ Author: shileicao(shileicao@stu.xjtu.edu.cn)
 Date: 17-2-26 上午10:51
 """
 
-import pprint
-
+# import pprint
 import pypinyin
 from elasticsearch import Elasticsearch
 
@@ -24,6 +23,14 @@ def hanzi2pinyin(word):
     ]
 
 
+def encode_pinyin(word):
+    pinyin_ = []
+    pinyin = hanzi2pinyin(word)
+    for ch in pinyin:
+        pinyin_.append('@'.join(list(pinyin)))
+    return '@@'.join(pinyin_)
+
+
 def create_index(id, doc):
     es.index(index="entity-index", doc_type='search', id=id, body=doc)
 
@@ -33,12 +40,12 @@ def refresh_index():
 
 
 def search_index(query_string, return_number=1):
-    query_pinyin = ' '.join(hanzi2pinyin(query_string))
+    query_pinyin = encode_pinyin(query_string)
     res1 = es.search(
         index='entity-index',
         doc_type='search',
         body={
-            'size': 2,
+            'size': return_number,
             'query': {
                 "query_string": {
                     'fields': ['Name'],
@@ -51,7 +58,7 @@ def search_index(query_string, return_number=1):
         index='entity-index',
         doc_type='search',
         body={
-            'size': 2,
+            'size': return_number,
             'query': {
                 "query_string": {
                     'fields': ['Pinyin'],
@@ -60,24 +67,18 @@ def search_index(query_string, return_number=1):
             }
         })
 
-    # res3 = es.search(index='entity-index', doc_type='search',
-    #                  body={'size': 2, 'query': {
-    #                     "bool": {"must": {"query_string": {"query": query_string}}}}})
-    #
-    #
-    # res4 = es.search(index='entity-index', doc_type='search',
-    #                  body={'size': 2,
-    #                        'query': {"match_phrase": {"Firstletterspace": {"query": ' '.join(query_string) + ' '}}}})
-
     answers = res1['hits']['hits'] + res2['hits']['hits']
 
     answers = sorted(
         answers, cmp=(lambda x, y: 1 if x['_score'] < y['_score'] else -1))
-    pprint.pprint(answers)
+    # pprint.pprint(answers)
     result_names = []
-    if return_number > 1:
+    entity_types = []
+    if return_number >= 1:
         for i in xrange(min(result_names, len(answers))):
             result_names.append(answers[i]['_source']['Name'])
+            entity_types.append(answers[i]['_source']['Entity_type'])
     else:
         result_names.append(answers[0]['_source']['Name'])
-    return result_names
+        entity_types.append(answers[0]['_source']['Entity_type'])
+    return result_names, entity_types
