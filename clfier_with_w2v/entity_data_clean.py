5d315450901e739b4be7359f6768e8af0bbd9f12
elias-1 <shileicao@stu.xjtu.edu.cn>
2017-03-06 20:24:59 +0800

    refine clean data

--- clfier_with_w2v/entity_data_clean.py
+++ clfier_with_w2v/entity_data_clean.py
@@ -12,24 +12,31 @@ Date: 17-3-6 下午6:49
 import csv
 import json
 import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 def processLine(data, sent2label, new_entity_out):
+    print len(sent2label)
     error_rows = []
     know_sent = set()
     for row in data:
         new_row = [item for item in row if item.strip()]
-
-        if new_row[0] in know_sent:
+        sent = new_row[0].decode('utf-8').strip()
+        if sent in know_sent:
             continue
         else:
-            know_sent.add(new_row[0])
-            if new_row[0] in sent2label:
-                new_row = [sent2label[new_row[0]]] + new_row
-                print new_row
+            know_sent.add(sent)
+            if sent in sent2label:
+                new_row = [sent2label[sent].encode('utf-8')] + new_row
                 new_entity_out.writerow(new_row)
+                sent2label.pop(sent)
             else:
                 error_rows.append(new_row)
+    print len(know_sent)
+
+    with open('../data/no_entity_tag.json', 'w') as f:
+        json.dump(sent2label, f, indent=4, ensure_ascii=False)
     return error_rows
 
 
