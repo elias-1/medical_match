4f883336ecf35d594fbe71f16bfaaabfc2c35d3b
elias-1 <shileicao@stu.xjtu.edu.cn>
2017-03-13 15:21:34 +0800

    fix bug

--- clfier_with_w2v/cnn_clfier_common2.py
+++ clfier_with_w2v/cnn_clfier_common2.py
@@ -32,7 +32,7 @@ tf.app.flags.DEFINE_string('cnn_clfier_common2_log_dir',
                            "cnn_clfier_common2_log", 'The log  dir')
 tf.app.flags.DEFINE_string("word2vec_path", "words_vec_100.txt",
                            "the word2vec data path")
-tf.app.flags.DEFINE_string("char2vec_path", "chars_vec_100.txt",
+tf.app.flags.DEFINE_string("char2vec_path", "chars_vec_50.txt",
                            "the char2vec data path")
 tf.app.flags.DEFINE_integer("max_sentence_len", MAX_SENTENCE_LEN,
                             "max num of tokens per query")
