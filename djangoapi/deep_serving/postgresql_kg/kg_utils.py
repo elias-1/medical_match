d945d1e7ad5e0be2d71fc262a2eb5d3c7ae35124
zhanmengting <476615360@qq.com>
2017-04-10 20:31:16 +0800

    post_sql

--- djangoapi/deep_serving/postgresql_kg/kg_utils.py
+++ djangoapi/deep_serving/postgresql_kg/kg_utils.py
@@ -19,6 +19,8 @@ config_file_dir = os.path.join(app_dir, 'config', 'config.conf')
 params = config(filename=config_file_dir, section='postgresql')
 conn = psycopg2.connect(**params)
 
+#conn = psycopg2.connect(host = 'localhost', database = 'kgdata', user = 'dbuser', password = '112233')
+
 
 def search_sql(sql):
     """ query parts from the parts table """
@@ -71,21 +73,25 @@ def kg_entity_summary(entities):
         success: 0/1
         """
     evalues = "','".join(entities)
-    sql = """SELECT distinct property_value 
-             FROM property 
-             WHERE property_name = 'desc' 
-             and entity_id in (
-                SELECT entity_id
-                FROM property 
-                WHERE property_name = 'name' 
-                and property_value IN ('%s'));"""
+    sql = """SELECT distinct a.property_value , b.property_value 
+             FROM property a left join property b 
+                on a.entity_id = b.entity_id
+             WHERE a.property_name = 'name' 
+                and a.property_value IN ('%s')
+                and b.property_name = 'desc' ;"""
     sql_result = search_sql(sql % (evalues))
     entitiy_summarys = []
     success = 0
+    summary_dict = {}
     if len(sql_result) > 0:
         success = 1
         for item in sql_result:
-            entitiy_summarys.append(item[0])
+            if not summary_dict.has_key(item[0]):
+                summary_dict[item[0]] = []
+            summary_dict[item[0]].append(item[1])
+    for key in summary_dict:
+        re_sent = key + '的概述：' + " \n ".join(summary_dict[key])
+        entitiy_summarys.append(re_sent)
     return entitiy_summarys, success
 
 
@@ -99,7 +105,26 @@ def kg_search_body_part(entities):
         body_part: list
         success: 0/1
     """
-    raise NotImplementedError
+    evalues = "','".join(entities)
+    sql = """SELECT distinct entity_name1, entity_name2 
+             FROM entity_relation
+             WHERE relation like '%%Body%%' 
+                and entity_name1 IN ('%s')
+             ORDER BY entity_name1;"""
+    sql_result = search_sql(sql % (evalues))
+    body_part = []
+    success = 0
+    body_dict = {}
+    if len(sql_result) > 0:
+        success = 1
+        for item in sql_result:
+            if not body_dict.has_key(item[0]):
+                body_dict[item[0]] = []
+            body_dict[item[0]].append(item[1])
+    for key in body_dict:
+        re_sent = key + '的部位：' + " , ".join(body_dict[key])
+        body_part.append(re_sent)
+    return body_part, success
 
 
 # TODO(zhanmengting)
@@ -109,10 +134,30 @@ def kg_search_price(entities):
     Args:
         entities: list
     Returns:
-        price: str
+        entity_price: str
         success: 0/1
     """
-    raise NotImplementedError
+    evalues = "','".join(entities)
+    sql = """SELECT distinct a.property_value , b.property_value 
+             FROM property a left join property b 
+                on a.entity_id = b.entity_id
+             WHERE a.property_name = 'name' 
+                and a.property_value IN ('%s')
+                and b.property_name = 'price' ;"""
+    sql_result = search_sql(sql % (evalues))
+    entitiy_price = []
+    success = 0
+    price_dict = {}
+    if len(sql_result) > 0:
+        success = 1
+        for item in sql_result:
+            if not price_dict.has_key(item[0]):
+                price_dict[item[0]] = []
+            price_dict[item[0]].append(item[1])
+    for key in price_dict:
+        re_sent = key + '的价格：' + " , ".join(price_dict[key])
+        entitiy_price.append(re_sent)
+    return entitiy_price, success
 
 
 # TODO(zhanmengting)
@@ -125,4 +170,23 @@ def kg_search_department(entities):
         department_list: list
         success: 0/1
     """
-    raise NotImplementedError
+    evalues = "','".join(entities)
+    sql = """SELECT distinct entity_name1, entity_name2 
+             FROM entity_relation
+             WHERE relation like '%%Dep%%' 
+                and entity_name1 IN ('%s')
+             ORDER BY entity_name1;"""
+    sql_result = search_sql(sql % (evalues))
+    department_list = []
+    success = 0
+    department_dict = {}
+    if len(sql_result) > 0:
+        success = 1
+        for item in sql_result:
+            if not department_dict.has_key(item[0]):
+                department_dict[item[0]] = []
+            department_dict[item[0]].append(item[1])
+    for key in department_dict:
+        re_sent = key + '的科室：' + " , ".join(department_dict[key])
+        department_list.append(re_sent)
+    return department_list, success
