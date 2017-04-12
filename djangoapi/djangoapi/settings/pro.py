7d3736697a7b98da831111acd6e7428237c5ce77
Elias-masker <shileicao@stu.xjtu.edu.cn>
2017-04-12 20:37:54 +0800

    add server config

---
+++ djangoapi/djangoapi/settings/pro.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 www.drcubic.com, Inc. All Rights Reserved
+#
+"""
+File: pro.py
+Author: shileicao(shileicao@stu.xjtu.edu.cn)
+Date: 2017/4/12 19:26
+python manage.py migrate –-settings=djangoapi.settings.pro
+"""
+from .base import *
+
+DEBUG = False
+
+# ADMINS = (
+#     ('Antonio M', 'antonio.mele@zenxit.com'),
+# )
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kgdata',
+        'USER': 'dbuser',
+        'PASSWORD': '112233',
+    }
+}
+
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
