ce0f25fd36668ae0e84291e1835cab21b54edc36
Elias-masker <shileicao@stu.xjtu.edu.cn>
2017-04-12 20:52:00 +0800

    add local setting

---
+++ djangoapi/djangoapi/settings/local.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 www.drcubic.com, Inc. All Rights Reserved
+#
+"""
+File: local.py
+Author: shileicao(shileicao@stu.xjtu.edu.cn)
+Date: 2017/4/12 19:24
+"""
+
+from .base import *
+
+DEBUG = True
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kgdata',
+        'USER': 'dbuser',
+        'PASSWORD': '112233',
+    }
+}
